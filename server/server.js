34f8d47d39edbb3cc2b1b9b40b392bd71ace6429
Shakshi2904 <132297826+Shakshi2904@users.noreply.github.com>
2025-02-12 12:37:26 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 app.use(cors({
-  origin: "https://assignment-ed9m.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true
-}));
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+  }));
 
 const PORT = process.env.PORT || 5000;
 app.get("/api/dashboard-data", async (req, res) => {
