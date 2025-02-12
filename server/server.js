df561ffcaa200aed94ddf11d4c494ca84ff2aeab
Shakshi2904 <132297826+Shakshi2904@users.noreply.github.com>
2025-02-12 12:56:43 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -5,10 +5,11 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
   }));
+  
 
 const PORT = process.env.PORT || 5000;
 app.get("/api/dashboard-data", async (req, res) => {
