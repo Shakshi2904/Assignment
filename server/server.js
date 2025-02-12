39d65b67ec908709b59facafeb9ec62e2462f6ab
Shakshi2904 <132297826+Shakshi2904@users.noreply.github.com>
2025-02-12 12:48:33 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
   }));
